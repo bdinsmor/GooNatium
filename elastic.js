21b6a402c1c63d7a0c056a9fdf955dae17f07e0a
Brian Dinsmore <akaronmexico7@gmail.com>
2020-02-02 09:04:40 +0100

    updated logic for google api

--- elastic.js
+++ elastic.js
@@ -7,17 +7,19 @@ require("dotenv").config();
 require("array.prototype.flatmap").shim();
 const client = new Client({
   cloud: {
-    id: "GooNatium:dXMtZWFzdC0xLmF3cy5mb3VuZC5pbyQwNTM1MzAxZjFhMTY0NDYyYTk1YWI1ODYxOWY2ZmRkOCQ0ZmJmZGVkMzY2M2M0NDFhOTVjMzBmY2Q5MzkxMzA0Zg=="
+    //id: "GooNatium:dXMtZWFzdC0xLmF3cy5mb3VuZC5pbyQwNTM1MzAxZjFhMTY0NDYyYTk1YWI1ODYxOWY2ZmRkOCQ0ZmJmZGVkMzY2M2M0NDFhOTVjMzBmY2Q5MzkxMzA0Zg=="
+    id: "placesearch:ZXUtY2VudHJhbC0xLmF3cy5jbG91ZC5lcy5pbyQ0Njc1MDRkNGY0OTY0MmNkOGY3ZDk5MmRjMDJjMDdhMSQ4Y2YxMTQ4ZTdkNjg0MDA2YmRkYmNjN2E4MmY2ZTkzOA=="
   },
   auth: {
     username: "elastic",
-    password: "s9jBhrfgIavPvIHYSG7fUPyb"
+    // password: "s9jBhrfgIavPvIHYSG7fUPyb"
+    password: process.env.ELASTICPW
   }
 });
 
 async function getGoogleDetails(name, location) {
   const coords = location.coordinates;
-  const fieldsToReturn = ["formatted_address", "name", "geometry", "permanently_closed", "place_id", "photos", "types", "plus_code"];
+  const fieldsToReturn = ["price_level", "rating", "user_ratings_total", "opening_hours","formatted_address", "name", "geometry", "permanently_closed", "place_id", "photos", "types", "plus_code"];
   // price_level, rating, user_ratings_total, opening_hours cost more money to get back...
 
   // using this search will only return hits where the OSM name is indexed within the Google data somewhere
@@ -87,41 +89,55 @@ async function run() {
     { ignore: [400] }
   );
   console.log("created locations index!");
-  let dataset = JSON.parse(fs.readFileSync("berlin2.geojson", "utf8"));
-  dataset = dataset.features;
-  const locations = [];
-  const max = 25;
-  if (dataset.length > max) {
-    dataset = dataset.slice(0, max);
+  let gooData = null;
+  try {
+    if (fs.existsSync("goonatim.json")) {
+       gooData = JSON.parse(fs.readFileSync("goonatim", "utf8"));
+    }
+  } catch (err) {
+    console.error(err);
   }
-  for (let i = 0; i < dataset.length; i++) {
-    if (!dataset[i].properties || !dataset[i].properties.name) {
-      continue;
-    }
-    const l = {
-      properties: dataset[i].properties,
-      doi: new Date(),
-      name: dataset[i].properties.name,
-      amenity: dataset[i].properties.amenity
-    };
-    if (dataset[i].id) {
-      l.id = dataset[i].id;
-    } else if (dataset[i].properties && dataset[i].properties["@id"]) {
-      l.id = dataset[i].properties["@id"];
-    }
+  if (!gooData) {
+    gooData = [];
+    // didnt find enriched google data locally, will reach out to google places API and grab it again
+      let dataset = JSON.parse(fs.readFileSync("berlin2.geojson", "utf8"));
+      dataset = dataset.features;
+      
+      const max = 25;
+      if (dataset.length > max) {
+        dataset = dataset.slice(0, max);
+      }
+      for (let i = 0; i < dataset.length; i++) {
+        if (!dataset[i].properties || !dataset[i].properties.name) {
+          continue;
+        }
+        const l = {
+          properties: dataset[i].properties,
+          doi: new Date(),
+          name: dataset[i].properties.name,
+          amenity: dataset[i].properties.amenity
+        };
+        if (dataset[i].id) {
+          l.id = dataset[i].id;
+        } else if (dataset[i].properties && dataset[i].properties["@id"]) {
+          l.id = dataset[i].properties["@id"];
+        }
 
-    if (dataset[i].geometry) {
-      l.location = dataset[i].geometry;
-      l.googleDetails = await getGoogleDetails(l.name, l.location);
+        if (dataset[i].geometry) {
+          l.location = dataset[i].geometry;
+          l.googleDetails = await getGoogleDetails(l.name, l.location);
+        }
+        console.log("finished: " + (i + 1));
+        gooData.push(l);
     }
-
-    console.log("finished " + i);
-
-    locations.push(l);
+    fs.writeFileSync("goonatim.json", JSON.stringify(gooData));
   }
 
-  console.log("# of records to index: " + location.length);
-  const body = locations.flatMap(doc => [{ index: { _index: "locations", _id: doc.id } }, doc]);
+
+  console.log("# of records to index: " + gooData.length);
+  
+  
+  const body = gooData.flatMap(doc => [{ index: { _index: "locations", _id: doc.id } }, doc]);
   const { body: bulkResponse } = await client.bulk({ refresh: true, body });
 
   if (bulkResponse.errors) {
